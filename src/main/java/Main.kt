7bf65dd6c0f33a4189a27652d3719057955decee
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:13:14 +0200

    One function to rule them all

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -2,6 +2,7 @@
 
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
+import java.net.URL
 
 fun parseAsJson(string: String): Map<String, Any?> =
     StringBuilder(string).let { Parser().parse(it) as JsonObject }.map
@@ -10,8 +11,20 @@ fun parseAsXML(string: String): Map<String, Any?> = emptyMap()
 
 fun parseAsHTML(string: String): Map<String, Any?> = emptyMap()
 
+fun dataFrom(url: URL, parserType: Int): Map<String, Any?> =
+    url.readText().let {
+        when (parserType) {
+            1 -> parseAsJson(it)
+            2 -> parseAsHTML(it)
+            3 -> parseAsXML(it)
+            else -> emptyMap()
+        }
+    }
+
 fun main(args: Array<String>) {
-    val jsonObject = parseAsJson("{\"data\": 42}")
+    val url = URL("https://api.github.com")
+
+    val jsonObject = dataFrom(url, 1)
 
     println(jsonObject)
 }
