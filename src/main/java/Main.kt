c0c8330030477be8854d34688123a908225d8a35
Alejandro Hernández <info@alejandrohdezma.com>
2017-09-26 23:28:06 +0200

    Yeah, this is so much better

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -4,30 +4,20 @@ import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import java.net.URL
 
-interface MyParser {
-    fun parse(string: String): Map<String, Any?>
-}
+fun parseAsJson(string: String): Map<String, Any?> =
+    StringBuilder(string).let { Parser().parse(it) as JsonObject }.map
 
-class JsonParser: MyParser {
-    override fun parse(string: String): Map<String, Any?> =
-        StringBuilder(string).let { Parser().parse(it) as JsonObject }.map
-}
+fun parseAsXML(string: String): Map<String, Any?> = emptyMap()
 
-class XMLParser: MyParser {
-    override fun parse(string: String): Map<String, Any?> = emptyMap()
-}
+fun parseAsHTML(string: String): Map<String, Any?> = emptyMap()
 
-class HTMLParser: MyParser {
-    override fun parse(string: String): Map<String, Any?> = emptyMap()
-}
-
-fun dataFrom(url: URL, parser: MyParser): Map<String, Any?> =
-    url.readText().let { parser.parse(it) }
+fun dataFrom(url: URL, parser: (String) -> Map<String, Any?>): Map<String, Any?> =
+    url.readText().let(parser)
 
 fun main(args: Array<String>) {
     val url = URL("https://api.github.com")
 
-    val jsonObject = dataFrom(url, JsonParser())
+    val jsonObject = dataFrom(url, ::parseAsJson)
 
     println(jsonObject)
 }
